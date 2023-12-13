ca2a6507e94f96ab85d34fc93a70297d9cf49f4d
2021pietcssachin148@poornima.org <2021pietcssachin148@poornima.org>
2023-12-13 23:33:51 +0530

    sec commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let level=0;
 let h2= document.querySelector('h2');
 let a= document.querySelector('a');
 
-document.addEventListener("keypress",function(){
+document.addEventListener("click",function(){
     if(started==false)
     {
         console.log("game is started");
